f828a4204dae519c69774d45f35aa1fc8455d398
LBcoding <41504139+LBcoding@users.noreply.github.com>
2018-07-22 00:11:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,27 +3,21 @@
 @author: Lorenzo Beretta
 """
 
-from setuptools import setup
+import setuptools
 
-
-setup(
+setuptools.setup(
     name='rkNN_imputer',
-    version='1.2',
+    version="1.2",
     author='Loernzo Beretta',
     author_email='lorberimm@hotmail.com',
-    url='https://github.com/LBcoding/ranking-kNN-imputation',
-    license='License :: OSI Approved :: MIT License',
     description='Ranking and kNN imputation algorithm',
     long_description='An imputation method based on ranking and nearest neighbor (kNN)',
-    packages=['rkNN_imputer'],
-    install_requires=['numpy', 'scipy', 'scikit-learn', 'skrebate'],
-    classifiers=[
-        'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3 :: Only',
-        'Topic :: Utilities'
-    ],
-    keywords=['machine learning', 'missing data', 'imputation'],
+    long_description_content_type="text/markdown",
+    url='https://github.com/LBcoding/ranking-kNN-imputation',
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
 )
