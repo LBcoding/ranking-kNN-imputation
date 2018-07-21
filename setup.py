0590d3eec22c449ee2f103429f936953fe8423f5
LBcoding <41504139+LBcoding@users.noreply.github.com>
2018-07-21 23:55:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,14 +18,13 @@ setup(
     zip_safe=False,
     packages=['rkNN_imputer']
     install_requires=['numpy', 'scipy', 'scikit-learn', 'skrebate'],
-    classifiers=[
-        'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3 :: Only',
-        'Topic :: Utilities'
-    ],
+    #classifiers=[
+    #    'Intended Audience :: Developers',
+    #    'Intended Audience :: Information Technology',
+    #    'Intended Audience :: Science/Research',
+    #    'License :: OSI Approved :: MIT License',
+    #    'Programming Language :: Python :: 3 :: Only',
+    #    'Topic :: Utilities'],
     keywords=['machine learning', 'missing data', 'imputation'],
     include_package_data=True,
 )
