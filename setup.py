45ffce0a81127c16db587b2ebb9851a77138b05b
LBcoding <41504139+LBcoding@users.noreply.github.com>
2018-07-21 23:28:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setup(
     description='Ranking and kNN imputation algorithm',
     long_description='An imputation method based on ranking and nearest neighbor (kNN).',
     zip_safe=False,
+    packages=['rkNN_imputer']
     install_requires=['numpy', 'scipy', 'scikit-learn', 'skrebate'],
     classifiers=[
         'Intended Audience :: Developers',
