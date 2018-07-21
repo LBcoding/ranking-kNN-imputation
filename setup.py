86ac4c0569dfee7e817b17765abc3b36872a18e6
LBcoding <41504139+LBcoding@users.noreply.github.com>
2018-07-21 23:27:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     license='License :: OSI Approved :: MIT License',
     description='Ranking and kNN imputation algorithm',
     long_description='An imputation method based on ranking and nearest neighbor (kNN).',
-    zip_safe=True,
+    zip_safe=False,
     install_requires=['numpy', 'scipy', 'scikit-learn', 'skrebate'],
     classifiers=[
         'Intended Audience :: Developers',
