498c71f31852ea13e010b43f21f5c96d85be071a
LBcoding <41504139+LBcoding@users.noreply.github.com>
2018-07-21 23:57:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,15 +16,16 @@ setup(
     description='Ranking and kNN imputation algorithm',
     long_description='An imputation method based on ranking and nearest neighbor (kNN).',
     zip_safe=False,
-    packages=['rkNN_imputer']
+    packages=['rkNN_imputer'],
     install_requires=['numpy', 'scipy', 'scikit-learn', 'skrebate'],
-    #classifiers=[
-    #    'Intended Audience :: Developers',
-    #    'Intended Audience :: Information Technology',
-    #    'Intended Audience :: Science/Research',
-    #    'License :: OSI Approved :: MIT License',
-    #    'Programming Language :: Python :: 3 :: Only',
-    #    'Topic :: Utilities'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Utilities'
+    ],
     keywords=['machine learning', 'missing data', 'imputation'],
     include_package_data=True,
 )
